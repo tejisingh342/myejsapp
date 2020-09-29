24194a726490e4a603e4ae833be50e70e73b707d
tejisingh342 <53550475+tejisingh342@users.noreply.github.com>
2020-09-29 16:35:13 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const db = require("../modules/db");
 var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "MyEjsApp" });
+  res.render("yess", { title: "MyEjsApp" });
 });
 
 // GET EMPLOYEE DATA
